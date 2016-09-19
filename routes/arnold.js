5826cfc479a1f151d52fabf7a1e1b8f17447a3fc
Dave Gravelle <dgravelle54@gmail.com>
2016-09-18 20:23:31 -0700

    added ssl

--- routes/arnold.js
+++ routes/arnold.js
@@ -5,9 +5,9 @@ const slackToken = process.env.SLACK_TOKEN;
 const commands = require('../commands');
 
 function tokenCheck(req, res, next) {
-    if(req.body.token !== slackToken) {
-        res.status(401)
-        res.send('Sorry, no token found. Try again with the right token ');
+    let token = req.body.token;
+    if(!token || token !== slackToken) {
+        res.status(401).send('Sorry, no token found. Try again with the right token ');
     }
     else {
         next()
@@ -19,18 +19,20 @@ function randomQuote(arr) {
     return arr[random]
 }
 
+router.use('/', tokenCheck);
+
 router.get('/', (req, res) => {
     res.status(200).send("I'll be back...");
 })
 
-router.post('/', tokenCheck, (req, res) => {
+router.post('/', (req, res) => {
     let response = {};
     let command = req.body.text;
     let arnoldSays;
 
     console.log();
     if (!command || command == 'help') {
-        // response.response_text = 'ephemeral'
+        response.response_text = 'ephemeral'
         arnoldSays = commands['help'];
     }
     else {
